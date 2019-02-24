0eb94eba170c9f93781e0e6256418c856d353010
Matthias Urlichs <matthias@urlichs.de>
2019-02-24 19:26:02 +0100

    uptick

--- collections.abc/setup.py
+++ collections.abc/setup.py
@@ -7,9 +7,9 @@ sys.path.append("..")
 import sdist_upip
 
 setup(name='micropython-collections.abc',
-      version='0.1.0',
+      version='0.2.0',
       description='collections.abc module for MicroPython',
-      long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
+      long_description="This is a port for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
       url='https://github.com/micropython/micropython-lib',
       author='micropython-lib Developers',
       author_email='micro-python@googlegroups.com',
