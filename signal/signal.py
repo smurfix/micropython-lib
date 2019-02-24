ce3acdcf1c001305cb0c6398f627b180be8ed0d4
Matthias Urlichs <matthias@urlichs.de>
2019-02-24 22:21:10 +0100

    signal: implement getsignal

--- signal/signal.py
+++ signal/signal.py
@@ -15,9 +15,17 @@ libc = ffilib.libc()
 signal_i = libc.func("i", "signal", "ii")
 signal_p = libc.func("i", "signal", "ip")
 
+_sigs = {}
+
 def signal(n, handler):
     if isinstance(handler, int):
+        _sigs[n] = handler
         return signal_i(n, handler)
     import ffi
     cb = ffi.callback("v", handler, "i")
+    _sigs[n] = cb
     return signal_p(n, cb)
+
+def getsignal(n):
+    return _sigs.get(n, SIG_DFL)
+
